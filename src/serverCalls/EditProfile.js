522d4f91e2a4ed109c066084f2f848cfbe11bb54
Adva <advaoren2015@gmail.com>
2024-03-06 19:55:30 +0200

    finish edit option

--- src/serverCalls/EditProfile.js
+++ src/serverCalls/EditProfile.js
@@ -1,21 +1,22 @@
 import axios from 'axios';
 
 
-export const editUserWImage = async (email) => {
-    const res = await axios.get(`http://localhost:8080/api/users/${email}`);
-    if (res.statusText == "OK") {
-        const user = await res.data; // Parse response body as JSON
-        if (user && Object.keys(user).length > 0) {
-            const newUrl = "data:image/png;base64," + user.img;
-            user.img = newUrl;
-            return user; // User found
-        } else {
-            return false // User not found
-        }
-    } else {
-        console.error('Error:', res.status);
-        return false;
-    }
+export const editUserWImage = async (id, email, firstName, lastName, password, token, profileImg) => {
+    await fetch((`http://localhost:8080/api/users/updAll/${id}`), {
+        "method": "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            'authorization': 'bearer ' + token // attach the token
+        },
+        body: JSON.stringify({
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: password,
+            userId: id,
+            img: profileImg
+        })
+    })
 }
 
 export const editUserNImage = async (id, email, firstName, lastName, password, token) => {
